f4f71de6932ef6627c8ec9fe47462c6f8f9a4c0d
tabascum <marques_vasco@yahoo.es>
2022-10-14 10:24:33 +0100

    Add styles to the overall page. Fix small screens responsiveness

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+//theme swich
+
+/* function switchTheme() {
+  document.body.classList.toggle("light-theme");
+  document.body.classList.toggle("dark-theme");
+}
+
+document.getElementById("switch").addEventListener("click", switchTheme); */
+
+//modal / dialog open and close functions
+
 const button = document.querySelector("#open-dialog");
 const modal = document.querySelector("dialog");
 const closeModal = document.querySelector("#close-dialog");
